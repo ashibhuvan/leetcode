dda37b57246e17ebe308ff538732c53d2d25215a
ashibhuvan <asishm.school@gmail.com>
2020-09-08 03:11:40 -0400

    leetcode 269

---
+++ 269.AlienDictionary.py
@@ -0,0 +1,23 @@
+
+from typing import List
+
+#test case
+word = ["wrt","wrf","er","ett","rftt"]
+class Solution:
+    def alienOrder(self,words:List[str]) -> str:
+
+        dict = {}
+        if len(words) == 1:
+            return words[0]
+        for idx,val in enumerate(words[0]):
+            dict.update({val:idx})
+
+        words = words[:1]
+        print(dict)
+        print(words)
+bob = Solution()
+print(bob.alienOrder(word))
+
+
+
+
